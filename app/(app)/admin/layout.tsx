72883ec4bf05fea4af2aa8848e2d84dd47b3a4ca
randseas <alchcraft24@gmail.com>
2025-02-21 13:32:23 +0300

    ad

--- app/(app)/admin/layout.tsx
+++ app/(app)/admin/layout.tsx
@@ -1,7 +1,19 @@
-import React from "react";
+"use client";
+import React, { useContext, useEffect } from "react";
 import Navbar from "../../../components/common/navbar";
+import { AppContext } from "../context";
+import toast from "react-hot-toast";
 
 export default function layout({ children }: { children: React.ReactNode }) {
+  const { state } = useContext(AppContext);
+  useEffect(() => {
+    if (!state.loading) {
+      if (state.userData.permission !== "admin") {
+        toast.error("Bu sayfaya erişmek için yönetici olmanız gerekmektedir");
+        return;
+      }
+    }
+  }, [state.loading]);
   return (
     <main className="!relative mt-[55px] md:mt-0 flex flex-row h-full w-full overflow-visible">
       <Navbar />
