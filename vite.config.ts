bdd85dc5298089733747e99bc7a8107f7240c011
randseas <alchcraft24@gmail.com>
2025-03-02 18:04:42 +0300

    AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,21 @@ import react from "@vitejs/plugin-react";
 import path from "node:path";
 
 export default defineConfig({
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          react: ["react", "react-dom", "react-router"],
+          ui: ["@mui/material", "@emotion/styled", "lucide-react"],
+          utils: ["clsx", "nprogress", "react-hot-toast"],
+          charts: ["react-apexcharts"],
+          network: ["axios", "socket.io-client", "ws"],
+          tailwind: ["tailwindcss", "tailwind-merge"],
+          syntax: ["react-syntax-highlighter"],
+        },
+      },
+    },
+  },
   plugins: [react()],
   resolve: {
     alias: {
